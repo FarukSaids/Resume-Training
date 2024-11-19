30686de138c4a49d64d87d9724631de0b7103cbb
Said Faruk <faruk.said@hotmail.com>
2024-11-19 23:08:41 +0300

    refactor:create urls in core app

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
-from DjangoResume.urls import urlpatterns
-from .views import index
+from .views import index,contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact', contact, name='contact'),
 ]
