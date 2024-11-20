c262010a2ffb175dd776181146b1bb21395cd2f2
SaidFaruk <faruk.said@hotmail.com>
2024-11-20 15:54:04 +0300

    refactor: create layout

--- DjangoResume/urls.py
+++ DjangoResume/urls.py
@@ -18,8 +18,14 @@ from xml.etree.ElementInclude import include
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    #bu dizindeki tüm dosyaların link ile erişilebilir olmasını sağlıyoruz
