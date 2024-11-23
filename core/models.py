39b4e2e86bf0fa37b113241921652d2c1bb76ca9
Said Faruk <faruk.said@hotmail.com>
2024-11-23 21:02:23 +0300

    feat:create general settings model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class GeneralSettings(models.Model): #database de bir tablo old belirtiliyor.
+    name = (models.CharField
+        (
+        default='',
+        max_length=254,
+        blank=True,
+    ))
+    description = models.CharField(
+        max_length=254,
+        default='',
+        blank=True,
+    )
+    parameter=models.CharField(
+        max_length=254,
+        default='',
+        blank=True,
+    )
+    updated_date =models.DateField(
+        blank=True,
+        auto_now=True,
+    )
+    created_date =models.DateField(
+        blank=True,
+        auto_now_add=True, #bir kere kayıt alıyor
+    )
